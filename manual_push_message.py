a8cc2c313780d8d21bf74ffe603006d9de043af1
yuu-eguci <yuu.eguci@gmail.com>
2023-08-01 15:53:53 +0900

    Update manual_push_message.py

--- manual_push_message.py
+++ manual_push_message.py
@@ -1,10 +1,10 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-CHANNEL_ACCESS_TOKEN = 'アクセストークン'
+LINE_CHANNEL_ACCESS_TOKEN = 'アクセストークン'
 USER_ID = 'userId'
 
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
 line_bot_api.push_message(
     USER_ID,
