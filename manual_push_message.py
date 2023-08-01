36acad6b1835313e572271fa0d5f60c4c33b44ca
yuu-eguci <yuu.eguci@gmail.com>
2023-08-01 15:56:59 +0900

    ✨ 環境変数化

--- manual_push_message.py
+++ manual_push_message.py
@@ -1,8 +1,9 @@
+import os
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-LINE_CHANNEL_ACCESS_TOKEN = 'アクセストークン'
-USER_ID = 'userId'
+LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+USER_ID = os.environ['USER_ID']
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
